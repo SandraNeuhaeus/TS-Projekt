a7b805441730bd08ecd8d1406fe729c30fe09ebb
Sandra Neuhäußer <sandra.neuhaeusser@gmx.net>
2021-09-01 10:05:35 +0200

    Relocated to new directory 'giza/'

--- run_giza.sh
+++
@@ -1,8 +0,0 @@
-cd giza-pp/GIZA++-v2
-./plain2snt.out '../europarl_data/Source' '../europarl_data/Target'
-cd ../mkcls-v2
-./mkcls -p'../europarl_data/Source' -V '../europarl_data/Source.vcb.classes'
-./mkcls -p'../europarl_data/Target' -V '../europarl_data/Target.vcb.classes'
-cd ../GIZA++-v2
-./snt2cooc.out '../europarl_data/Source.vcb' '../europarl_data/Target.vcb' '../europarl_data/Source_Target.snt' > '../europarl_data/Source_Target.cooc'
-./GIZA++ -S '../europarl_data/Source.vcb' -T '../europarl_data/Target.vcb' -C '../europarl_data/Source_Target.snt' -CoocurrenceFile '../europarl_data/Source_Target.cooc' -o Result -outputpath '../europarl_data/output'
